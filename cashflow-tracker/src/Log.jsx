cffa73579adffebf6a2227c0bfd8d7acbc40d785
Francesco Valentini <fv26012001@gmail.com>
2023-02-11 02:20:24 +0100

    Improve select box

--- cashflow-tracker/src/Log.jsx
+++ cashflow-tracker/src/Log.jsx
@@ -35,18 +35,26 @@ export default class Log extends Component {
           <label>
             Source:
             <select name="source" id="source">
-              <option value="wallet">Wallet</option>
-              <option value="bcc">BCC</option>
-              <option value="intesa_sanpaolo">Intesa Sanpaolo</option>
-              <option value="unicredit">Unicredit</option>
-              <option value="bper">BPER</option>
-              <option value="pocket">Pocket</option>
-              <option value="n26">N26</option>
-              <option value="satispay">Satispay</option>
-              <option value="paypal">PayPal</option>
-              <option value="hype">Hype</option>
-              <option value="revolut">Revolut</option>
-              <option value="extra">Extra</option>
+              <optgroup label="Cash">
+                <option value="wallet">Wallet</option>
+                <option value="pocket">Pocket</option>
+              </optgroup>
+              <optgroup label="Digital">
+                <option value="satispay">Satispay</option>
+                <option value="paypal">PayPal</option>
+                <option value="hype">Hype</option>
+                <option value="revolut">Revolut</option>
+              </optgroup>
+              <optgroup label="Banks">
+                <option value="bcc">BCC</option>
+                <option value="intesa_sanpaolo">Intesa Sanpaolo</option>
+                <option value="unicredit">Unicredit</option>
+                <option value="bper">BPER</option>
+                <option value="n26">N26</option>
+              </optgroup>
+              <optgroup label="Other">
+                <option value="extra">Extra</option>
+              </optgroup>
               {/* Add option in settings to change this list completely, with a reset to default button */}
             </select>
           </label>
